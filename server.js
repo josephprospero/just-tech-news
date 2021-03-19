3a0a43133b114cd790681ab90efcbac0db9bf5ba
Joseph Prospero <jprospero413@gmail.com>
2021-03-18 18:57:19 -0700

    added partials and added jest for testing

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const path = require('path');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-const session = require('express-session');
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
